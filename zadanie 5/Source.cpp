7ad7af8a05d81e844ff75fafa70dcea28f0ef945
Elecrto <53616280+Elecrto@users.noreply.github.com>
2021-12-19 13:36:19 +0300

    Add files via upload

---
+++ zadanie 5/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+bool palindrom(const char* str){
+    int len = strlen(str);
+
+    for (int i = 0; i < len / 2; i++)
+        if (str[i] != str[len - 1 - i])
+            return false;
+
+    return true;
+}
+
+int main(){
+    setlocale(LC_ALL, "Rus");
+    char* str = new char[256];
+    cin >> str;
+
+    if (palindrom(str) == true) { cout << "Палиндром"; }
+    else { cout << "Не палиндром"; }
+    delete[] str;
+    return 0;
+}
